50e4f349155084dc593b95416f584cd4c447b813
royalflushever <royalflushdiamond918@gmail.com>
2023-05-10 08:27:51 -0500

    add test cases

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -37,71 +37,71 @@ describe('App', () => {
     fetch.mockReset();
   });
 
-  // it('renders the app title', () => {
-  //   render(
-  //     <BrowserRouter>
-  //       <App />
-  //     </BrowserRouter>
-  //   );
-  //   expect(screen.getByText('Invoice Manager')).toBeInTheDocument();
-  // });
+  it('renders the app title', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getByText('Invoice Manager')).toBeInTheDocument();
+  });
 
-  // it('fetches and displays the invoices', async () => {
-  //   render(
-  //     <BrowserRouter>
-  //       <App />
-  //     </BrowserRouter>
-  //   );
-  //   expect(fetch).toHaveBeenCalledWith(`${baseURL}/invoices`);
-  //   // expect(fetch).toHaveBeenCalledTimes(1);
-  //   await waitFor(() => screen.getByText('INV-001'));
-  //   expect(screen.getByText('INV-001')).toBeInTheDocument();
-  //   expect(screen.getByText('$1000')).toBeInTheDocument();
-  //   expect(screen.getByText('2023-05-31')).toBeInTheDocument();
-  //   expect(screen.getByText('created')).toBeInTheDocument();
-  //   expect(screen.getByText('INV-002')).toBeInTheDocument();
-  //   expect(screen.getByText('$2000')).toBeInTheDocument();
-  //   expect(screen.getByText('2023-06-30')).toBeInTheDocument();
-  //   expect(screen.getByText('approved')).toBeInTheDocument();
-  // });
+  it('fetches and displays the invoices', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(fetch).toHaveBeenCalledWith(`${baseURL}/invoices`);
+    // expect(fetch).toHaveBeenCalledTimes(1);
+    await waitFor(() => screen.getByText('INV-001'));
+    expect(screen.getByText('INV-001')).toBeInTheDocument();
+    expect(screen.getByText('$1000')).toBeInTheDocument();
+    expect(screen.getByText('2023-05-31')).toBeInTheDocument();
+    expect(screen.getByText('created')).toBeInTheDocument();
+    expect(screen.getByText('INV-002')).toBeInTheDocument();
+    expect(screen.getByText('$2000')).toBeInTheDocument();
+    expect(screen.getByText('2023-06-30')).toBeInTheDocument();
+    expect(screen.getByText('approved')).toBeInTheDocument();
+  });
 
-  // it('creates a new invoice', async () => {
-  //   const file = new File(['invoice scan'], 'invoice.pdf', { type: 'application/pdf' });
-  //   fetch.mockResolvedValueOnce({
-  //     json: () =>
-  //       Promise.resolve({
-  //         id: 3,
-  //         number: 'INV-003',
-  //         amount: 3000,
-  //         due_date: '2023-07-31',
-  //         status: 'created',
-  //         scan: file,
-  //       }),
-  //   });
+  it('creates a new invoice', async () => {
+    const file = new File(['invoice scan'], 'invoice.pdf', { type: 'application/pdf' });
+    fetch.mockResolvedValueOnce({
+      json: () =>
+        Promise.resolve({
+          id: 3,
+          number: 'INV-003',
+          amount: 3000,
+          due_date: '2023-07-31',
+          status: 'created',
+          scan: file,
+        }),
+    });
 
-  //   render(
-  //     <BrowserRouter>
-  //       <App />
-  //     </BrowserRouter>
-  //   );
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
 
-  //   await userEvent.click(screen.getByText('Create Invoice'));
-  //   userEvent.type(screen.getByLabelText('Invoice #.'), 'INV-003');
-  //   userEvent.type(screen.getByLabelText('Amount'), '3000');
-  //   userEvent.type(screen.getByLabelText('Due Date'), '2023-07-31');
-  //   const fileInput = screen.getByTestId('dropzone');
-  //   Object.defineProperty(fileInput, 'files', {
-  //     value: [file],
-  //   });
-  //   fireEvent.drop(fileInput);
-  //   expect(await screen.findByText('invoice.pdf')).toBeInTheDocument();
-  //   userEvent.click(screen.getByText('Submit'));
-  //   expect(fetch).toHaveBeenCalledWith(`${baseURL}/invoices`, expect.any(Object));
-  //   await waitFor(() => screen.getByText('Invoice created successfully.'));
-  //   expect(screen.getByText('Invoice created successfully.')).toBeInTheDocument();
-  // });
+    await userEvent.click(screen.getByText('Create Invoice'));
+    userEvent.type(screen.getByLabelText('Invoice #.'), 'INV-003');
+    userEvent.type(screen.getByLabelText('Amount'), '3000');
+    userEvent.type(screen.getByLabelText('Due Date'), '2023-07-31');
+    const fileInput = screen.getByTestId('dropzone');
+    Object.defineProperty(fileInput, 'files', {
+      value: [file],
+    });
+    fireEvent.drop(fileInput);
+    expect(await screen.findByText('invoice.pdf')).toBeInTheDocument();
+    userEvent.click(screen.getByText('Submit'));
+    expect(fetch).toHaveBeenCalledWith(`${baseURL}/invoices`, expect.any(Object));
+    await waitFor(() => screen.getByText('Invoice created successfully.'));
+    expect(screen.getByText('Invoice created successfully.')).toBeInTheDocument();
+  });
 
-  it('updates an invoice', async () => {
+  it('close the purchased invoice', async () => {
     fetch.mockResolvedValueOnce({
       json: () =>
         Promise.resolve({
@@ -133,4 +133,17 @@ describe('App', () => {
     expect(screen.getByText('closed')).toBeInTheDocument();
     expect(screen.getByText('Invoice Inv-002 is closed successfully.')).toBeInTheDocument();
   });
+
+  it('should not approve the created invoice', async () => {
+  });
+
+  it('should not purchase the rejected invoice', async () => {
+  });
+
+  it('should purchase the approved invoice', async () => {
+  });
+
+  it('should reject the created invoice', async () => {
+  });
+
 });
